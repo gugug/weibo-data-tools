4a4ec0c2c011ad6f4aa1a10673883d19749045ee
gugug <734093894@qq.com>
2017-11-10 15:23:29 +0800

    测试mongodb的crud方法

---
+++ src/test/java/gdufs/iiip/weibo/data/tools/age8sex/CRUDManagerTest.java
@@ -0,0 +1,64 @@
+package gdufs.iiip.weibo.data.tools.age8sex;
+
+import gdufs.iiip.weibo.data.dao.MongoHelper;
+import org.bson.Document;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by gu on 2017/11/10.
+ */
+public class CRUDManagerTest {
+    MongoHelper mongoHelper = new MongoHelper("weibo", "add", 27017);
+    CRUDManager crudManager = new CRUDManager(mongoHelper, "age");
+
+    @Test
+    public void insert() throws Exception {
+        Map<String, Object> ageMap = new HashMap<String, Object>();
+        ageMap.put("_id", 311);
+        ageMap.put("a79", 1283);
+        ageMap.put("a80", 728);
+        ageMap.put("a90", 1502);
+        ageMap.put("a95", 2303);
+        ageMap.put("anull", 4181);
+        crudManager.insert(ageMap);
+        crudManager.closeMongoClient();
+    }
+
+    @Test
+    public void insertMany() throws Exception {
+        Map<String, Object> ageMap = new HashMap<String, Object>();
+        List<Document> docList = new ArrayList<Document>();
+        docList.add(new Document(ageMap));
+        crudManager.insertMany(docList);
+    }
+
+    @Test
+    public void deleteOne() throws Exception {
+    }
+
+    @Test
+    public void deleteMany() throws Exception {
+    }
+
+    @Test
+    public void updateMany() throws Exception {
+    }
+
+    @Test
+    public void updateOne() throws Exception {
+    }
+
+    @Test
+    public void printAll() throws Exception {
+    }
+
+    @Test
+    public void closeMongoClient() throws Exception {
+    }
+
+}
