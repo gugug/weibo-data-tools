4ee236efb600d9273f25cdb2982e3ba2be511bf6
gugug <734093894@qq.com>
2017-11-12 20:29:53 +0800

    全部工具类的测试方法

--- src/test/java/gdufs/iiip/weibo/data/tools/alltest/AllToolsTest.java
+++ src/test/java/gdufs/iiip/weibo/data/tools/alltest/AllToolsTest.java
@@ -1,15 +1,20 @@
-package gdufs.iiip.weibo.data.tools.others;
+package gdufs.iiip.weibo.data.tools.alltest;
 
 import gdufs.iiip.weibo.data.config.DaoConfig;
 import gdufs.iiip.weibo.data.config.PathConfig;
+import gdufs.iiip.weibo.data.tools.age.AgeUtil;
 import gdufs.iiip.weibo.data.tools.chinamap.ChinaMapUtil;
+import gdufs.iiip.weibo.data.tools.sex.SexUtil;
 import gdufs.iiip.weibo.data.tools.topic.TopicJson;
-import gdufs.iiip.weibo.data.tools.transformtree.TransformTree;
+import gdufs.iiip.weibo.data.tools.transformtree.TransformTreeUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by gu on 2017/11/10.
  */
-public class TestTools {
+public class AllToolsTest {
 
     public static void main(String[] args) {
         testTransform();
@@ -19,11 +24,11 @@ public class TestTools {
         String transformtxtpath = "C:\\Users\\gu\\Desktop\\incident_path2.txt";
         String eventName = "杭州保姆纵火";
         String save = "C:\\Users\\gu\\Desktop";
-        String transformJson = TransformTree.genTransformJson(eventName, transformtxtpath, save, "transform1.json");
+        String transformJson = TransformTreeUtil.genTransformJson(eventName, transformtxtpath, save, "transform1.json");
         System.out.println(transformJson);
     }
 
-    public static void testChina() {
+    public static void testChinaMap() {
         DaoConfig daoConfig = new DaoConfig("weibo", "127.0.0.1", 27017);
         String areapath = "/media/iiip/Elements/WeiboData/WeiboNew/教师殴打环卫/area.txt";
         String eid = "594";
@@ -38,6 +43,22 @@ public class TestTools {
     public static void testPath() {
         PathConfig pathConfig = new PathConfig("/home/iiip/java/workspace/weibo/WeiboNewsProject/WebContent/");
         System.out.println(pathConfig.getCHARACTER_TXT_PATH());
-
     }
+
+    public void testInsertAge() throws Exception {
+        DaoConfig daoConfig = new DaoConfig("aa", "sss", 33);
+        Map<String, Object> ageMap = new HashMap<String, Object>();
+        ageMap.put("_id", 311);
+        ageMap.put("a79", 1283);
+        AgeUtil.insertAge(daoConfig, ageMap);
+    }
+
+    public void testInsertSex() throws Exception {
+        DaoConfig daoConfig = new DaoConfig("aa", "sss", 33);
+        Map<String, Object> sexMap = new HashMap<String, Object>();
+        sexMap.put("_id", 311);
+        sexMap.put("girl", 1283);
+        SexUtil.insertSex(daoConfig, sexMap);
+    }
+
 }
