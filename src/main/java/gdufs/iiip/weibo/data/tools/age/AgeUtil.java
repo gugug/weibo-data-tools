19708945be007ed4bdc7ab8e9e2cac1388641f27
gugug <734093894@qq.com>
2017-11-12 20:09:33 +0800

    年龄表的插入工具

---
+++ src/main/java/gdufs/iiip/weibo/data/tools/age/AgeUtil.java
@@ -0,0 +1,33 @@
+package gdufs.iiip.weibo.data.tools.age;
+
+import gdufs.iiip.weibo.data.config.DaoConfig;
+import gdufs.iiip.weibo.data.dao.CRUDManager;
+import gdufs.iiip.weibo.data.dao.MongoHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by gu on 2017/11/12.
+ */
+public class AgeUtil {
+
+    public static void insertAge(DaoConfig daoConfig, Map<String, Object> ageMap) {
+        MongoHelper mongoHelper = new MongoHelper(daoConfig);
+        CRUDManager crudManager = new CRUDManager(mongoHelper, "age");
+        crudManager.insert(ageMap);
+        crudManager.closeMongoClient();
+    }
+
+    public static void main(String[] args) {
+        DaoConfig daoConfig = new DaoConfig("aa", "sss", 33);
+        Map<String, Object> ageMap = new HashMap<String, Object>();
+        ageMap.put("_id", 311);
+        ageMap.put("a79", 1283);
+        ageMap.put("a80", 728);
+        ageMap.put("a90", 1502);
+        ageMap.put("a95", 2303);
+        ageMap.put("anull", 4181);
+        insertAge(daoConfig, ageMap);
+    }
+}
