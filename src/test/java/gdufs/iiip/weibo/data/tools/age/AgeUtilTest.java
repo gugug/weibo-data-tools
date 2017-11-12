f4c14f2e15ce1deb70a65b5c01f1fae8c8de4ced
gugug <734093894@qq.com>
2017-11-12 20:12:25 +0800

    年龄表的插入工具测试类

---
+++ src/test/java/gdufs/iiip/weibo/data/tools/age/AgeUtilTest.java
@@ -0,0 +1,28 @@
+package gdufs.iiip.weibo.data.tools.age;
+
+import gdufs.iiip.weibo.data.config.DaoConfig;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gu on 2017/11/12.
+ */
+public class AgeUtilTest {
+    @Test
+    public void insertAge() throws Exception {
+        DaoConfig daoConfig = new DaoConfig("aa", "sss", 33);
+        Map<String, Object> ageMap = new HashMap<String, Object>();
+        ageMap.put("_id", 311);
+        ageMap.put("a79", 1283);
+        ageMap.put("a80", 728);
+        ageMap.put("a90", 1502);
+        ageMap.put("a95", 2303);
+        ageMap.put("anull", 4181);
+        AgeUtil.insertAge(daoConfig, ageMap);
+    }
+
+}
